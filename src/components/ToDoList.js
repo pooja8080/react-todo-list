2196516f8d65a5481336a48b0e5533197d824ddb
poojasri <panigrahi.pooja-s@metlife.com>
2019-05-15 17:01:01 +0530

    changes in todolist

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,11 +3,29 @@ import ToDoItem from './ToDoItem';
 
 class ToDoList extends Component {
   render() {
+    const {items, clearList, handleDelete, handleEdit} = this.props;
     return (
-      <section>
-        <h1>Hi there list</h1>
-        <ToDoItem />
-        </section>
+      <ul className="list-group my-5">
+       <h3 className="text-captalize text-center">Todo List</h3>
+            {items.map(item =>{return (
+            <ToDoItem 
+            key={item.id} 
+            title={item.title}
+            handleDelete={() => handleDelete(item.id)}
+            handleEdit={()=> handleEdit(item.id)}
+            />
+            );
+            })}
+          <button
+          type="button"
+          className="btn btn-danger
+          btn-block text-uppercase 
+          mt-5"
+          onClick={clearList}
+          >
+          ClearList
+          </button>
+      </ul>
     )
   }
 }
