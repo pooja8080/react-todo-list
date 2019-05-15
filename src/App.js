b95650206f54fe22afda47d5444f6e8f1ce03edb
poojasri <panigrahi.pooja-s@metlife.com>
2019-05-15 12:34:12 +0530

    adding the functionality

--- src/App.js
+++ src/App.js
@@ -6,13 +6,49 @@ import ToDoList from './components/ToDoList';
 //comments to check the githib setup
 //blahh
 class App extends Component{
+  constructor(props){
+    super(props);
+    state={
+      items:[{id:1,title: 'wake up'},
+      {id:2,title: 'make breakfast'}],
+      id: uuid(),
+      item: '',
+      editItem: false
+    }
+  }
+  handleChange = (e) => {
+    console.log('handle change');
+  }
+  handleSubmit = (e) => {
+    console.log('handle submit');
+  }
+  clearList = () =>{
+    console.log('clearlist');
+  }
+  hanldeDelete = (id) =>{
+    console.log(`handle delete ${id}`);
+  }
+  hanldeEdit = (id) =>{
+    console.log(`handle edit ${id}`);
+  }
+
   render(){
   return (
-    <div>
       <div className="container">
         <div className="row">
-        <ToDoInput/>
-        <ToDoList />
+        <div className=".col-10.mx-auto.col-md-8.mt-5">
+        <h3 className="text-capitalize text center">ToDoInput</h3>
+        <ToDoInput 
+        item={this.state.item} 
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        editItem={this.state.editItem}/>
+        <ToDoList
+        items={this.state.items} 
+        clearList={this.clearList}
+        handleDelete={this.hanldeDelete}
+        hanldeEdit={this.hanldeEdit}
+        />
         </div>
       </div>
     </div>
